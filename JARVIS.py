2d0743ae8c6bc833789739fac62672669f6bd667
Monish Paul <monish.paul2000@gmail.com>
2020-08-20 13:50:01 +0530

    version1 Jarvis

---
+++ JARVIS.py
@@ -0,0 +1,26 @@
+import pyttsx3
+import datetime
+import speechRecognition as sr
+
+engin=pyttsx3.init('sapi5')
+voices=engin.getProperty('voices')
+#print(voices[1].id)
+engin.setProperty('voice',voices[1].id)
+
+
+def speak(audio):
+    engin.say(audio)
+    engin.runAndWait()
+def wishMe():
+    time=datetime.datetime.now().hour
+    if(time>=0 and time<12):
+        speak("Good Morning!!")
+    elif(time>=12 and time<18):
+        speak("Good Afternoon!!")
+    else:
+        speak("Good Evening!!")
+    speak("I am Jarvis Sir. Please tell me how may I help you")
+
+if __name__ == '__main__':
+    wishMe();
+    #speak("monish is good boy")
